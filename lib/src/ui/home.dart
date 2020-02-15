0bdaede4febc7073b3a1136ea433d91a120c9838
fathan21 <42806997+fathan21@users.noreply.github.com>
2020-02-16 01:46:21 +0700

    dsf
    fdsf

--- lib/src/ui/home.dart
+++ lib/src/ui/home.dart
@@ -14,34 +14,36 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String _activemenu = 'Catatan';
+  String _activemenu = '1';
   List _selectedNote = [];
   bool _selectedNoteActive = false;
   @override
   void initState() {
     super.initState();
   }
-  void _setSelectedNote(id)async {
-    var idx = _selectedNote.indexOf(id); 
-    if (idx >-1){
+
+  void _setSelectedNote(id) async {
+    var idx = _selectedNote.indexOf(id);
+    if (idx > -1) {
       _selectedNote.removeAt(idx);
-    }else{
+    } else {
       _selectedNote.add(id);
     }
     _selectedNoteActive = _selectedNote.length > 0 ? true : false;
     setState(() {
-      _selectedNote = _selectedNote; 
+      _selectedNote = _selectedNote;
       _selectedNoteActive = _selectedNoteActive;
     });
   }
+
   void _removeSelectedNote() async {
     setState(() {
-      _selectedNote = []; 
+      _selectedNote = [];
       _selectedNoteActive = false;
     });
   }
 
-  void _setActiveMenu(e) async{
+  void _setActiveMenu(e) async {
     setState(() {
       _activemenu = e;
     });
@@ -50,121 +52,146 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: _selectedNoteActive ? null : Drawer(
-          child: ListDrawer(
-              activeMenu: _activemenu, setActiveMenu: _setActiveMenu
-          )
-      ),
+      drawer: _selectedNoteActive
+          ? null
+          : Drawer(
+              child: ListDrawer(
+                  activeMenu: _activemenu, setActiveMenu: _setActiveMenu),
+            ),
       appBar: AppBar(
-        title: _selectedNoteActive ? Row(
-          children: <Widget>[
-            IconButton(
-              icon: Icon(Icons.close),
-              onPressed: _removeSelectedNote,
-            ),
-            Text(_selectedNote.length.toString())
-          ],
-        ) : Text(widget.title),
-        actions: _selectedNoteActive? 
-          <Widget>[
-            
-            IconButton(
-              icon: const Icon(Icons.alarm_add),
-              tooltip: 'Alarm',
-              onPressed: () {
-            
-              },
-            ),
-            IconButton(
-              icon: new Icon(Icons.archive),
-              tooltip: 'Archive',
-              onPressed: () {
-            
-              },
-            ),
-            IconButton(
-              icon: new Icon(Icons.delete),
-              tooltip: 'Delete',
-              onPressed: () {
-            
-              },
-            ),
-          ] : 
-          <Widget>[
-            IconButton(
-              icon: Icon(Icons.add_circle),
-              onPressed: () {
-                dialogAddNote(context);
-              },
-            ),
-            IconButton(
-              icon: Icon(Icons.search),
-              onPressed: () {},
-            )
-          ],
+        title: _selectedNoteActive
+            ? Row(
+                children: <Widget>[
+                  IconButton(
+                    icon: Icon(Icons.close),
+                    onPressed: _removeSelectedNote,
+                  ),
+                  Text(_selectedNote.length.toString())
+                ],
+              )
+            : Text(widget.title),
+        actions: _selectedNoteActive
+            ? <Widget>[
+                IconButton(
+                  icon: const Icon(Icons.alarm_add),
+                  tooltip: 'Alarm',
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: new Icon(Icons.archive),
+                  tooltip: 'Archive',
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: new Icon(Icons.delete),
+                  tooltip: 'Delete',
+                  onPressed: () {},
+                ),
+              ]
+            : <Widget>[
+                IconButton(
+                  icon: Icon(Icons.add_circle),
+                  onPressed: () {
+                    dialogAddNote(context);
+                  },
+                ),
+                IconButton(
+                  icon: Icon(Icons.search),
+                  onPressed: () {},
+                )
+              ],
       ),
-      body: WidgetCatatan(
-        setSelectedNote: _setSelectedNote,
-        removeSelectedNote: _removeSelectedNote,
-        selectedNoteActive: _selectedNoteActive,
-        selectedNote: _selectedNote,
-      )
+      body: _buildBody(),
     );
+  }
+
+  Widget _buildNote() {
+    return WidgetCatatan(
+      activeMenu: _activemenu,
+      setSelectedNote: _setSelectedNote,
+      removeSelectedNote: _removeSelectedNote,
+      selectedNoteActive: _selectedNoteActive,
+      selectedNote: _selectedNote,
+    );
+  }
+
+  Widget _buildBody() {
+    var data;
+    switch (_activemenu) {
+      case "5":
+        return Text(_activemenu.toString());
+        break;
+      case "6":
+        return Text(_activemenu.toString());
+        break;
+
+      default:
+        data = _buildNote();
+    }
+
+    return data;
   }
 }
 
 class WidgetCatatan extends StatelessWidget {
   final setSelectedNote;
   final removeSelectedNote;
-
+  final activeMenu;
   final List selectedNote;
   final bool selectedNoteActive;
-  
+
   WidgetCatatan(
-    {this.selectedNote,this.selectedNoteActive, this.setSelectedNote, this.removeSelectedNote}
-  );
-  
+      {
+        this.activeMenu,
+        this.selectedNote,
+        this.selectedNoteActive,
+        this.setSelectedNote,
+        this.removeSelectedNote
+      }
+    );
+
   @override
   Widget build(BuildContext context) {
     NotesBloc _notesBloc = BlocProvider.of<NotesBloc>(context);
     return Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Expanded(
-              // The streambuilder allows us to make use of our streams and display
-              // that data on our page. It automatically updates when the stream updates.
-              // Whenever you want to display stream data, you'll use the StreamBuilder.
-              child: StreamBuilder<List<NoteComp>>(
-                stream: _notesBloc.notes,
-                builder: (BuildContext context,
-                    AsyncSnapshot<List<NoteComp>> snapshot) {
-                  // Make sure data exists and is actually loaded
-                  // // print(snapshot.hasError);
-                  if (snapshot.hasData) {
-                    // If there are no notes (data), display this message.
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+            // The streambuilder allows us to make use of our streams and display
+            // that data on our page. It automatically updates when the stream updates.
+            // Whenever you want to display stream data, you'll use the StreamBuilder.
+            child: StreamBuilder<List<NoteComp>>(
+              stream: _notesBloc.notes,
+              builder: (BuildContext context,
+                  AsyncSnapshot<List<NoteComp>> snapshot) {
+                // Make sure data exists and is actually loaded
+                // // print(snapshot.hasError);
+                if (snapshot.hasData) {
+                  // If there are no notes (data), display this message.
 
-                    if (snapshot.data.length == 0) {
-                      return GridNoteEmptyWidget();
-                    }
-
-                    List<NoteComp> notes = snapshot.data;
-                    return GridNoteListWidget(notes,
-                        selectedNote: selectedNote,
-                        setSelectedNote: setSelectedNote,
-                        selectedNoteActive: selectedNoteActive,
-                        removeSelectedNote: removeSelectedNote,
-                    );
+                  if (snapshot.data.length == 0) {
+                    return GridNoteEmptyWidget();
                   }
-                  return Center(
-                    child: CircularProgressIndicator(),
+
+                  List<NoteComp> notes = snapshot.data;
+                  return GridNoteListWidget(
+                    notes,
+                    selectedNote: selectedNote,
+                    setSelectedNote: setSelectedNote,
+                    selectedNoteActive: selectedNoteActive,
+                    removeSelectedNote: removeSelectedNote,
                   );
-                },
-              ),
+                }
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              },
             ),
-          ],
-        ),
-      );
+          ),
+        ],
+      ),
+    );
   }
 }
 
@@ -178,42 +205,45 @@ class ListDrawer extends StatelessWidget {
         new TextStyle(fontSize: 18.0, fontWeight: FontWeight.w400);
     BoxDecoration menuStyle = BoxDecoration();
     BoxDecoration activeMenuStyle = BoxDecoration(
-        color: Colors.brown[200],
-        borderRadius: BorderRadius.only(
-            topRight: Radius.circular(30.0),
-            bottomRight: Radius.circular(30.0)));
+      color: Colors.brown[200],
+      borderRadius: BorderRadius.only(
+        topRight: Radius.circular(30.0),
+        bottomRight: Radius.circular(30.0),
+      ),
+    );
     List<Widget> list = new List();
     for (var item in menu) {
-      list.add(Container(
-        margin: EdgeInsets.only(right: 10.0, bottom: 5.0),
-        decoration: activeMenu.toString().toLowerCase() ==
-                item["label"].toString().toLowerCase()
-            ? activeMenuStyle
-            : menuStyle,
-        child: ListTile(
-          leading: Icon(item["icon"]),
-          title: Text(
-            item["label"],
-            style: menuTextStyle,
+      list.add(
+        Container(
+          margin: EdgeInsets.only(right: 10.0, bottom: 5.0),
+          decoration: activeMenu == item["id"] ? activeMenuStyle : menuStyle,
+          child: ListTile(
+            leading: Icon(item["icon"]),
+            title: Text(
+              item["label"],
+              style: menuTextStyle,
+            ),
+            // trailing: Icon(Icons.arrow_forward),
+            onTap: () {
+              setActiveMenu(item["id"]);
+              Navigator.pop(context);
+              //
+            },
           ),
-          // trailing: Icon(Icons.arrow_forward),
-          onTap: () {
-            setActiveMenu(item["label"].toString().toString());
-            Navigator.pop(context);
-            //
-          },
         ),
-      ));
+      );
     }
 
     return Container(
       padding: EdgeInsets.only(bottom: 20.0, top: 10.0),
       decoration: BoxDecoration(
-          border: Border(
-              bottom: BorderSide(
-        color: borderBottom == 1 ? Colors.grey : Colors.transparent,
-        width: 1.0,
-      ))),
+        border: Border(
+          bottom: BorderSide(
+            color: borderBottom == 1 ? Colors.grey : Colors.transparent,
+            width: 1.0,
+          ),
+        ),
+      ),
       child: Column(
         children: list,
       ),
@@ -229,23 +259,24 @@ class ListDrawer extends StatelessWidget {
       color: ThemeData().primaryColor,
       child: Text(
         "Notepad",
-        style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold, color: Colors.white),
+        style: TextStyle(
+            fontSize: 30.0, fontWeight: FontWeight.bold, color: Colors.white),
       ),
     ));
     // activeMenu, menu,borderBottom
     var menu = [
-      {"label": "Catatan", "icon": Icons.book},
-      {"label": "Pengingat", "icon": Icons.notifications},
+      {"id": "1", "label": "Catatan", "icon": Icons.book},
+      {"id": "2", "label": "Pengingat", "icon": Icons.notifications},
     ];
 
     var menu1 = [
-      {"label": "Arsip", "icon": Icons.archive},
-      {"label": "Sampah", "icon": Icons.delete},
+      {"id": "3", "label": "Arsip", "icon": Icons.archive},
+      {"id": "4", "label": "Sampah", "icon": Icons.delete},
     ];
 
     var menu2 = [
-      {"label": "Setelan", "icon": Icons.settings},
-      {"label": "Bantuan & Masukan", "icon": Icons.help_outline},
+      {"id": "5", "label": "Setelan", "icon": Icons.settings},
+      {"id": "6", "label": "Bantuan & Masukan", "icon": Icons.help_outline},
     ];
     list.add(_options(
       context,
